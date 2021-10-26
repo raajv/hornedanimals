f1cbc091e7e26d821cb1ff0fcda562acd7cc626e
raajv <raaj.vardhan@gmail.com>
2021-10-26 15:26:40 +0530

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+
+class Footer extends Component{
+  render(){
+    return(
+      <>
+      <h1>AUTHOR: Raajvardhan Chaukulkar</h1>
+      </>
+    )
+  }
+}
+export default Footer
