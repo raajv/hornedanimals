7570198ee5006db8543c2c53a8df12e288d62b49
raajv <raaj.vardhan@gmail.com>
2021-10-28 13:38:39 +0530

    work in progress for lab 3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import Hornedbeasts from './Hornedbeasts.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
+  
   render() {
     return (
       <div>
         
+        
         <Header/>
-        <Main/>
+        <Main />
         <Footer/>
       </div>
     )
--- src/Hornedbeasts.js
+++ src/Hornedbeasts.js
@@ -1,22 +1,38 @@
 import React ,{Component} from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import SelectedBeast from './selectedbeasts.js';
 
 class Hornedbeasts extends Component{
   constructor(props){
     super(props);
-      this.state= {noOfClicks : 0};
+      this.state= {noOfClicks : 0,
+                    show:false
+                  }
     }
     
     handleClick =() =>{
       this.setState({noOfClicks : this.state.noOfClicks+1});
-      console.log ('was clicked');
+      ;
     }
+
+    handleClickSize = () =>{
+      this.setState({show:true});
+      console.log ('was clicked')
+    }
+
+
+    hideModal =() =>{
+      this.setState({show:false});
+    }
+
+
+
   render(){
     return (
       <>
     <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={this.props.beast.image_url } alt={this.props.beast.title} />
+  <Card.Img variant="top" src={this.props.beast.image_url }onClick={this.handleClickSize} alt={this.props.beast.title} />
   <Card.Body>
     <Card.Title>{this.props.beast.title}</Card.Title>
     <Card.Text>
@@ -27,7 +43,7 @@ class Hornedbeasts extends Component{
   </Card.Body>
 </Card>
      
-      
+      <SelectedBeast show={this.state.show} />
       
       
       
