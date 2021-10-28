c6913782e9584634fbc8573ce8d90c7b724853f5
raajv <raaj.vardhan@gmail.com>
2021-10-28 23:14:38 +0530

    changed to change state of app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-import Hornedbeasts from './Hornedbeasts.js';
+import Horneddata from './data.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SelectedBeast from './selectedbeasts.js';
 export default class App extends Component {
-  
+  constructor(props){
+    super(props);
+      this.state= {show:false,
+      featuredBeast :{}}
+    }
+    hideModal =() =>{
+      this.setState({show:false});
+    }
+    showModal = () =>{
+      this.setState({show:true});
+    }
+    updateBeast =(beast)=>{
+      this.setState({
+        featuredBeast : beast
+      })
+        this.showModal();
+      
+    }
   render() {
     return (
       
@@ -16,7 +33,8 @@ export default class App extends Component {
         
         
         <Header/>
-        <Main />
+        <Main updateBeast={this.updateBeast} hornedData={Horneddata}/>
+        <SelectedBeast featuredBeast={this.state.featuredBeast} show={this.state.show} hideModal={this.hideModal}showModal={this.showModal}/>
         <Footer/>
       </div>
     )
