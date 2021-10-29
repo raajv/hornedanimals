25c747f1cbd450e9a14b5000e3535006b58d2650
raajv <raaj.vardhan@gmail.com>
2021-10-29 20:25:08 +0530

    lab04 in progress

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Footer from './Footer.js';
 import Horneddata from './data.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SelectedBeast from './selectedbeasts.js';
+
+let data = Horneddata.map(beast=>beast= beast.horns)
+
+
 export default class App extends Component {
+  
   constructor(props){
     super(props);
       this.state= {show:false,
-      featuredBeast :{}}
-    }
+      featuredBeast :{},
+      allHorns : data,
+    }}
     hideModal =() =>{
       this.setState({show:false});
     }
@@ -22,17 +28,59 @@ export default class App extends Component {
         featuredBeast : beast
       })
         this.showModal();
-      
-    }
-  render() {
+      }
+
+      newbeast = ()=>{
+        let newBeast= {};
+       let  newData={}
+       newData=  Horneddata.includes(this.state.allHorns)
+       newBeast.push({newData});
+        return newBeast;}
+        
+      handleHorn =(e) =>{
+        let selection = e.target.value;
+        let updatedHornArray
+        let newBeast= {};
+          let  newData={}
+      if (selection === "1"){
+          updatedHornArray = data.filter(horns=>horns===1);
+          this.setState ({allhorns : updatedHornArray})
+          newData=  Horneddata.includes(this.state.allHorns)
+          newBeast.push({newData});
+         return newBeast;
+        }
+          
+          else if (selection === "2"){
+            updatedHornArray = data.filter(horns=>horns===2);
+            this.setState ({allhorns : updatedHornArray})
+            newData=  Horneddata.includes(this.state.allHorns)
+          newBeast.push({newData});
+         return newBeast;
+            }
+          else if (selection === "3"){
+              updatedHornArray = data.filter(horns=>horns===3);
+              this.setState ({allhorns : updatedHornArray})
+              newData=  Horneddata.includes(this.state.allHorns)
+          newBeast.push({newData});
+         return newBeast;
+              }
+          else if (selection === "more than 3"){
+                updatedHornArray = data.filter(horns=>horns>3);
+                this.setState ({allhorns : updatedHornArray})
+                newData=  Horneddata.includes(this.state.allHorns)
+          newBeast.push({newData});
+         return newBeast;
+          }
+          
+        };
+       
+        render() {
     return (
       
       
       <div  style={{
         backgroundColor: '#1E90FF'}}>
-        
-        
-        <Header/>
+        <Header beastData ={this.handleHorn} hornNumb ={this.state.allHorns} newBeast={this.newBeast}/>
         <Main updateBeast={this.updateBeast} hornedData={Horneddata}/>
         <SelectedBeast featuredBeast={this.state.featuredBeast} show={this.state.show} hideModal={this.hideModal}showModal={this.showModal}/>
         <Footer/>
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,44 @@
 import { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import BeastData from './data.json'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+
+
+
 class Header extends Component{
+ 
   render(){
     return(
       <Navbar bg="dark" variant="dark"fixed="top">
     <Container>
+      <Form>
+      <Form.Select onChange={this.props.beastData} aria-label="Default select example">
+            <option>Select a number filter</option>
+            <option value="1">one</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="more than 3">more than 3</option>
+     </Form.Select>
+    </Form>
+    <ListGroup>
+          <ListGroup.Item>
+          <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={this.props.newBeast.image_url } alt={this.props.newBeast.title} />
+  <Card.Body>
+    <Card.Title>{this.props.newBeast.title}</Card.Title>
+    <Card.Text>
+    {this.props.newBeast.description}
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+          </ListGroup.Item>
+        </ListGroup>
     <Navbar.Brand href="#home">Horned Beasts</Navbar.Brand>
     </Container>
   </Navbar>
